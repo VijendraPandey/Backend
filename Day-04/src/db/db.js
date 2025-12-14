48ca944ff6b00428ef1e78acc89f220277f0bff5
Vijendra Pandey <vijendra@Vijendras-MacBook-Air.local>
2025-12-14 13:22:38 +0530

    updated Day-04

--- Day-04/src/db/db.js
+++ Day-04/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDB = async () => {
-    await mongoose.connect('mongodb+srv://Vijendra:rsefTaCA1LuSsemB@cluster002.am7judj.mongodb.net/Backend');
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('Connected to MongoDB');
 };
 
